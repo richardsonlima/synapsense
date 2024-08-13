3bf42fc074f5dc9b350af1884ecddd42ec49252b
Richardson Lima <contatorichardsonlima@gmail.com>
2024-08-13 10:41:34 -0300

    adding tests

---
+++ examples/tests/test_pyicl.py
@@ -0,0 +1,19 @@
+import unittest
+from pyicl import ContextManager, PromptBuilder
+
+class TestPyICL(unittest.TestCase):
+    def test_add_and_retrieve_context(self):
+        cm = ContextManager()
+        cm.add_context("test", ["example1", "example2"])
+        self.assertEqual(cm.get_context("test"), ["example1", "example2"])
+
+    def test_prompt_builder(self):
+        cm = ContextManager()
+        cm.add_context("test", ["example1", "example2"])
+        pb = PromptBuilder(cm)
+        prompt = pb.build_prompt("test", "input1")
+        expected_prompt = "Example: example1\nExample: example2\nUser Input: input1\nModel Output:"
+        self.assertEqual(prompt, expected_prompt)
+
+if __name__ == '__main__':
+    unittest.main()
